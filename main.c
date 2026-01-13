2f2df5618ed2b6bfda0248c6df576aa600d5f3ef
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 17:49:59 +0300

    Implement strict proximity constraint logic for decimal generation
    Added source code that produces random decimals in the normalized range [0.2, 1.2]. The algorithm utilizes a validation loop to ensure every generated element maintains a difference of <= 0.1 relative to the entire existing dataset.

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int i,n,uygun,j;
+    double sayi,dizi[999],fark;
+
+    srand(time(NULL));
+
+    printf("n i giriniz");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        for(;;){
+            sayi=(rand()%101+20)/100.0;
+
+            uygun=1;
+
+            for(j=0;j<i;j++){
+
+                if(dizi[j]>sayi){
+                    fark=dizi[j]-sayi;
+                }
+                else{
+                    fark=sayi-dizi[j];
+                }
+
+                if(fark>0.1){
+                    uygun=0;
+                    break;
+                }
+
+            }
+            if(uygun==1){
+                break;
+            }
+
+
+
+        }
+        dizi[i]=sayi;
+        printf("%lf \n",dizi[i]);
+    }
+}
